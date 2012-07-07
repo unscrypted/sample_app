930b63f6acc318fd2aaab86f41cd43779e9c094d
Unscrypted <rcd1214@hotmail.com>
2012-07-07 02:31:09 -0700

    finished user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp::Application.routes.draw do
 
   root to: 'static_pages#home'
+  resources :users
 
   match '/help',     to: 'static_pages#help'
   match '/about',    to: 'static_pages#about'
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name			"Ryan Dodd"
+		email			"rcd1214@hotmail.com"
+		password	"Kewlio14"
+		password_confirmation		"Kewlio14"
+	end
+end
