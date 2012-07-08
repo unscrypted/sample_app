b081dc1a38ff6c5c686b90637352ad5144e08f5f
Unscrypted <rcd1214@hotmail.com>
2012-07-08 15:23:12 -0700

    Cleaned up signup page html and added some tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,14 +5,24 @@ describe "UserPages" do
   subject { page }
 
   describe "signup" do
-  	before { visit signup_path }
+  	
+    before { visit signup_path }
 
   	let(:submit) { "Create my account" }
 
   	describe "with invalid information" do
-  		it "should not create a user" do
+  		
+      it "should not create a user" do
   			expect { click_button submit }.not_to change(User, :count)
   		end
+      
+      describe "after submission" do
+        
+        before { click_button submit }
+
+        it { should have_selector('title', text: 'Sign Up') }
+        it { should have_content('error') }
+      end
   	end
 
   	describe "with valid information" do
@@ -26,6 +36,14 @@ describe "UserPages" do
   		it "should create a user" do
   			expect { click_button submit }.to change(User, :count).by(1)
   		end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Congratulations') }     
+      end
   	end
   end
 
