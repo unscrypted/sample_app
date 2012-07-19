782aa9623c481fa0db002cb4b24ed8da76340c68
Unscrypted <rcd1214@hotmail.com>
2012-07-19 01:53:49 -0700

    Finished user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
-	factory :user do
-		name			"Ryan Dodd"
-		email			"rcd1214@hotmail.com"
-		password	"Kewlio14"
-		password_confirmation		"Kewlio14"
-	end
+  factory :user do
+    sequence(:name)     { |n| "Person #{n}" }
+    sequence(:email)    { |n| "person_#{n}@example.com" }
+    password "foobar"
+    password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
+  end
 end
