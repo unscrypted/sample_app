32bbe6edc227f76cfd5971ab9522c5c66002c524
Ryan Dodd <rcd1214@hotmail.com>
2012-08-02 02:54:50 -0700

    added relationships model

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,15 @@ SampleApp::Application.routes.draw do
 
   root to: 'static_pages#home'
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
   match '/help',     to: 'static_pages#help'
   match '/about',    to: 'static_pages#about'
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "UserPages" do
+describe "User pages" do
 
   subject { page }
 
@@ -146,6 +146,34 @@ describe "UserPages" do
       it { should have_link('Sign out', href: signout_path) }
       specify { user.reload.name.should == new_name }
       specify { user.reload.email.should == new_email }
+    end
+  end
+
+  describe "following/followers" do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:other_user) { FactoryGirl.create(:user) }
+    before { user.follow!(other_user) }
+
+    describe "followed users" do
+      before do
+        sign_in user
+        visit following_user_path(user)
+      end
+
+      it { should have_selector('title', text: full_title('Following')) }
+      it { should have_selector('h3', text: 'Following') }
+      it { should have_link(other_user.name, href: user_path(other_user)) }
+    end
+
+    describe "followers" do
+      before do
+        sign_in other_user
+        visit followers_user_path(other_user)
+      end
+
+      it { should have_selector('title', text: full_title('Followers')) }
+      it { should have_selector('h3', text: 'Followers') }
+      it { should have_link(user.name, href: user_path(user)) }
     end
   end
 end
